3a3c52c51b707c3c4f18ae031ec80d0a2a2cf959
Rojan Rajbhandari <rrojan20@gmail.com>
2022-03-07 00:03:30 +0545

    Add total cost to controller instance variables

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -5,5 +5,9 @@ class CartController < ApplicationController
   before_action :authenticate_user!, only: [:index]
   def index
     @cart_items = current_user.cart_items
+    @total_cost = 0
+    @cart_items.each do |item|
+      @total_cost += item.product.price.to_i * item.quantity
+    end
   end
 end
