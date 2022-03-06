4f7f9a0ba00a9260215f084c4ca0d03eb9f6e374
Rojan Rajbhandari <rrojan20@gmail.com>
2022-03-06 13:15:50 +0545

    Change items to be fetched from db

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,10 @@
 # frozen_string_literal: true
 
+# Controller for generic pages for the site
 class PagesController < ApplicationController
-  def home; end
+  def home
+    @necklaces = Product.necklaces(limit: 4)
+    @sunglasses = Product.sunglasses(limit: 4)
+    @earrings = Product.earrings(limit: 4)
+  end
 end
