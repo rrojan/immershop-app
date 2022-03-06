f173ec524d5ce63f590b8152ad90e8ebdf97c68f
Rojan Rajbhandari <rrojan20@gmail.com>
2022-03-06 23:53:03 +0545

    Add card controller index

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -1,5 +1,9 @@
 # frozen_string_literal: true
 
+# Controller for user cart
 class CartController < ApplicationController
-  def index; end
+  before_action :authenticate_user!, only: [:index]
+  def index
+    @cart_items = current_user.cart_items
+  end
 end
