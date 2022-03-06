caccc66926afca56423604e8008cb582780292c6
Rojan Rajbhandari <rrojan20@gmail.com>
2022-03-06 23:11:40 +0545

    Add cart resource and functionality

---
+++ app/controllers/cart_controller.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+class CartController < ApplicationController
+  def index; end
+end
---
+++ db/migrate/20220306155104_create_carts.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+class CreateCarts < ActiveRecord::Migration[7.0]
+  def change
+    create_table :carts do |t|
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
