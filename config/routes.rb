50514c446cd9c74d35fb3904a9daea3e35294af1
Rojan Rajbhandari <rrojan20@gmail.com>
2022-03-06 18:28:51 +0545

    Route product description page

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
   devise_for :users
 
   get 'tryon/:product_id', to: 'tryons#show', as: 'try_on'
+  get 'products/:id', to: 'products#show', as: 'product'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 end
