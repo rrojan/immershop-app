39490fb563d1f9681373a68ad08eea643653abc6
Rojan Rajbhandari <rrojan20@gmail.com>
2022-03-06 23:11:55 +0545

    Add cart related actions

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,15 +2,27 @@
 
 # Controller for the products resource
 class ProductsController < ApplicationController
-  before_action :set_product, only: %i[show buy]
+  before_action :set_product, only: %i[show]
+  before_action :authenticate_user!, only: [:add_to_cart]
 
   def show; end
 
-  def add_to_cart; end
+  def add_to_cart
+    create_user_cart_item(params[:id].to_i, params[:quantity])
+  end
+
+  def buy
+    create_user_cart_item(params[:id].to_i, params[:quantity])
+    redirect_to cart_path
+  end
 
   private
 
   def set_product
-    @product = Product.find(params[:id])
+    @product = Product.find(params[:id].to_i)
+  end
+
+  def create_user_cart_item(id, quant)
+    current_user.cart.cart_items.create(product_id: id, quantity: quant)
   end
 end
